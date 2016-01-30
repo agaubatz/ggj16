a62bbe4e7d8b958a1e6461339bf98278e7d6ccc9
Daniel Epstein <depstein@cs.washington.edu>
2016-01-30 11:48:58 -0800

    import witch & follow camera

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+	public Witch witch;
+	public Umbrella umbrella;
+	//public const float dampTime = 0.15f;
+	//private Vector3 velocity = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(transform.position.x + 1f*Time.deltaTime, transform.position.y, transform.position.z);
+	}
+}
