19528a3e58dc5ca0c8bf77584a41c6648a8a095c
Daniel Epstein <depstein@cs.washington.edu>
2016-01-30 14:09:48 -0800

    Witch easing

--- Assets/Scripts/Witch.cs
+++ Assets/Scripts/Witch.cs
@@ -2,13 +2,13 @@
 using System.Collections;
 
 public class Witch : MonoBehaviour {
-	private float timeSinceUpdate = 0f;
 	private const float UPDATEEVERY = 2f;
-	private const float MAXDISTFROMCAMERA = 6f;
+	private const float MAXDISTFROMCAMERA = 5.5f;
 	private const float SPEEDSTDEV = 0.5f;
+	private float timeSinceUpdate = UPDATEEVERY; //So it updates frame 1
 	private float speed = FollowCamera.CAMERASPEED;
-	private Vector3 velocity = Vector3.zero;
-	private const float DAMP = 0.05f;
+	private float oldSpeed = FollowCamera.CAMERASPEED;
+	private const float DAMP = 1f;
 
 	// Use this for initialization
 	void Start () {
@@ -22,12 +22,19 @@ public class Witch : MonoBehaviour {
 			timeSinceUpdate = 0;
 			float destX = transform.localPosition.x;
 			float cameraX = Camera.main.transform.position.x + UPDATEEVERY * FollowCamera.CAMERASPEED;
+			oldSpeed = speed;
+			int rerollLimit = 10;
 			do {
 				speed = RandomFromDistribution.RandomNormalDistribution (FollowCamera.CAMERASPEED, SPEEDSTDEV);
 				destX = transform.localPosition.x + speed * UPDATEEVERY;
-			} while (Mathf.Abs(destX - cameraX) > MAXDISTFROMCAMERA);
+				rerollLimit--;
+			} while (Mathf.Abs(destX - cameraX) > MAXDISTFROMCAMERA && rerollLimit > 0);
 		}
-		//transform.localPosition = Vector3.SmoothDamp(transform.localPosition, new Vector3(transform.localPosition.x + speed*Time.deltaTime, transform.localPosition.y, transform.localPosition.z), ref velocity, DAMP);
-		transform.localPosition = new Vector3(transform.localPosition.x + speed*Time.deltaTime, transform.localPosition.y, transform.localPosition.z);
+
+		float mySpeed = speed;
+		if (timeSinceUpdate < DAMP) {
+			mySpeed = oldSpeed + (speed-oldSpeed)*(float)Easing.QuadEaseInOut(timeSinceUpdate, 0, 1, DAMP);
+		}
+		transform.localPosition = new Vector3(transform.localPosition.x + mySpeed*Time.deltaTime, transform.localPosition.y, transform.localPosition.z);
 	}
 }
