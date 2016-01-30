f81194b7fd1c7904d57f7ef5dd6062c3696fa587
Daniel Epstein <depstein@cs.washington.edu>
2016-01-30 09:55:32 -0800

    basic umbrella/witch

---
+++ Assets/Scripts/Umbrella.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Umbrella : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		transform.position =  new Vector3(mouseScreenPosition.x, mouseScreenPosition.y, 0);
+	}
+}
---
+++ Assets/Scripts/Witch.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Witch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition = new Vector2(transform.localPosition.x + 1f*Time.deltaTime, transform.localPosition.y);
+	}
+}
