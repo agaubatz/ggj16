e5770b826913cd2c6a4295a70f9ebf3fa8819674
Daniel Epstein <depstein@cs.washington.edu>
2016-01-30 23:16:12 -0800

    Evil birdzzzzz

--- Assets/Scripts/Umbrella.cs
+++ Assets/Scripts/Umbrella.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum UmbrellaState {
+	Normal, Large, Disabled
+}
+
 public class Umbrella : MonoBehaviour {
 	private float powerUpDuration = 0f;
 	private Vector3 oldScale = Vector3.zero;
-	private bool inactive = false;
+	private UmbrellaState state = UmbrellaState.Normal;
 
 	// Use this for initialization
 	void Start () {
@@ -13,10 +17,16 @@ public class Umbrella : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (powerUpDuration > 0) {
+		if (state != UmbrellaState.Normal && powerUpDuration > 0) {
 			powerUpDuration -= Time.deltaTime;
 			if (powerUpDuration < 0) {
-				transform.localScale = oldScale;
+				if (state == UmbrellaState.Disabled) {
+					GetComponentInChildren<Collider2D> ().enabled = true;
+					SpriteRenderer renderer = GetComponentInChildren<SpriteRenderer> ();
+					renderer.color = new Color(1f, 1f, 1f, 1f);
+				} else if(state == UmbrellaState.Large) {
+					transform.localScale = oldScale;
+				}
 			}
 		}
 		Vector3 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -25,8 +35,15 @@ public class Umbrella : MonoBehaviour {
 
 	public void CollideWithBird(Bird b) {
 		if (b.isGood) {
+			state = UmbrellaState.Large;
 			powerUpDuration = 4f;
 			transform.localScale = new Vector3 (2f * oldScale.x, oldScale.y, oldScale.z);
+		} else {
+			powerUpDuration = 2f;
+			state = UmbrellaState.Disabled;
+			GetComponentInChildren<Collider2D> ().enabled = false;
+			SpriteRenderer renderer = GetComponentInChildren<SpriteRenderer> ();
+			renderer.color = new Color(1f, 1f, 1f, 0.5f);
 		}
 	}
 }
