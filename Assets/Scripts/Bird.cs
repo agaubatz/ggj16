914dfae38d7fc0690e998d61a24a71db936cd74b
Daniel Epstein <depstein@cs.washington.edu>
2016-01-30 21:35:19 -0800

    Bird sprites

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bird : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition = new Vector3(transform.localPosition.x - 5f*Time.deltaTime, transform.localPosition.y, transform.localPosition.z);
+	}
+}
---
+++ Assets/Scripts/BirdManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BirdManager : MonoBehaviour {
+	public static BirdManager instance;
+	public GameObject BirdPrefab;
+	private List<Bird> birds = new List<Bird>();
+	private const float summonEvery = 10f;
+	private float lastSummon = 0f;
+
+	void Awake() {
+		instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lastSummon += Time.deltaTime;
+		if (lastSummon >= summonEvery) {
+			lastSummon = 0f;
+			SummonBird();
+		}
+	}
+
+	void SummonBird() {
+		float x = FollowCamera.instance.JustOffscreen.x + BirdPrefab.GetComponent<SpriteRenderer> ().bounds.extents.x;
+		float y = RandomFromDistribution.RandomNormalDistribution (3f, 0.5f);
+		Vector3 summonPosition = new Vector3(x, y, BirdPrefab.transform.position.z);
+		Bird b = ((GameObject)Instantiate (BirdPrefab, summonPosition, Quaternion.identity)).GetComponent<Bird> ();
+		birds.Add(b);
+	}
+}
