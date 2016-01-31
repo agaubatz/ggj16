18a4a354781955dea4dc417c782f16bb9cea00c6
Daniel Epstein <depstein@cs.washington.edu>
2016-01-30 17:50:16 -0800

    witch stating stuff

--- Assets/Scripts/Witch.cs
+++ Assets/Scripts/Witch.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum WitchState {
+	Spellcasting, Walking
+}
+
 public class Witch : MonoBehaviour {
 	public ScoreManager score;
 	private const float UPDATEEVERY = 2f;
 	private const float MAXDISTFROMCAMERA = 5.5f;
 	private const float SPEEDSTDEV = 0.5f;
+	private const float DAMP = 1f;
 	private float timeSinceUpdate = UPDATEEVERY; //So it updates frame 1
 	private float speed = FollowCamera.CAMERASPEED;
 	private float oldSpeed = FollowCamera.CAMERASPEED;
-	private const float DAMP = 1f;
+	private WitchState state = WitchState.Walking;
 
 	private Animator animator;
 
@@ -23,17 +28,7 @@ public class Witch : MonoBehaviour {
 		timeSinceUpdate += Time.deltaTime;
 		if (timeSinceUpdate >= UPDATEEVERY) {
 			timeSinceUpdate = 0;
-			float destX = transform.localPosition.x;
-			float cameraX = Camera.main.transform.position.x + UPDATEEVERY * FollowCamera.CAMERASPEED;
-			oldSpeed = speed;
-			int rerollLimit = 10;
-			do {
-				speed = RandomFromDistribution.RandomNormalDistribution (FollowCamera.CAMERASPEED, SPEEDSTDEV);
-				destX = transform.localPosition.x + speed * UPDATEEVERY;
-				rerollLimit--;
-			} while (Mathf.Abs(destX - cameraX) > MAXDISTFROMCAMERA && rerollLimit > 0);
-			animator.SetFloat ("speed", speed);
-			score.score += 5;
+			UpdateState();
 		}
 
 		float mySpeed = speed;
@@ -47,5 +42,33 @@ public class Witch : MonoBehaviour {
 		}
 
 		transform.localPosition = new Vector3(transform.localPosition.x + mySpeed*Time.deltaTime, transform.localPosition.y, transform.localPosition.z);
+	}
+
+	void UpdateState() {
+		if (state == WitchState.Spellcasting) { //You successfully cast the spell!
+			score.score += 5;
+		}
+
+		float rand = Random.value;
+		float cameraXdest = Camera.main.transform.position.x + UPDATEEVERY * FollowCamera.CAMERASPEED;
+		if (rand < 0.2f && cameraXdest - transform.localPosition.x < MAXDISTFROMCAMERA) { //20% of idle spellcasting, unless it's going to put us off screen
+			Debug.Log("Spellcasting");
+			state = WitchState.Spellcasting;
+			speed = 0;
+			animator.SetFloat("speed", 0);
+		} else {
+			Debug.Log("Walking");
+			state = WitchState.Walking;
+			float destX = transform.localPosition.x;
+			float cameraX = Camera.main.transform.position.x + UPDATEEVERY * FollowCamera.CAMERASPEED;
+			oldSpeed = speed;
+			int rerollLimit = 10;
+			do {
+				speed = RandomFromDistribution.RandomNormalDistribution (FollowCamera.CAMERASPEED*1.2f, SPEEDSTDEV);
+				destX = transform.localPosition.x + speed * UPDATEEVERY;
+				rerollLimit--;
+			} while (Mathf.Abs(destX - cameraX) > MAXDISTFROMCAMERA && rerollLimit > 0);
+			animator.SetFloat ("speed", speed);
+		}
 	}
 }
