e827b8f15f5ee49bfecbe950a6ccdb9bf10bf97d
Daniel Epstein <depstein@cs.washington.edu>
2016-01-30 18:12:38 -0800

    multiple witches screeching in pain

--- Assets/Scripts/RainManager.cs
+++ Assets/Scripts/RainManager.cs
@@ -2,10 +2,10 @@
 using System.Collections;
 
 public class RainManager : MonoBehaviour {
-	public Witch witch;
 	const float RAINHEIGHT = 10;
 	const int NUMRAINDROPS = 100;
 	const float DAMAGETHRESHOLD = .25f;
+	public WitchManager witches;
 
 	// Use this for initialization
 	void Start () {
@@ -14,21 +14,23 @@ public class RainManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Bounds witchBounds = witch.GetComponent<BoxCollider2D>().bounds;
-		int numCollided = 0;
-		float total = 0;
-		for(float x = witchBounds.min.x; x <= witchBounds.max.x; x += witchBounds.extents.x/NUMRAINDROPS) {
-			total++;
-			RaycastHit2D hit = Physics2D.Raycast (new Vector2 (x, RAINHEIGHT), Vector2.down);
-			if(hit.collider != null && hit.collider.gameObject == witch.gameObject) {
-				numCollided++;
+		foreach (Witch witch in witches.Witches) {
+			Bounds witchBounds = witch.GetComponent<BoxCollider2D>().bounds;
+			int numCollided = 0;
+			float total = 0;
+			for(float x = witchBounds.min.x; x <= witchBounds.max.x; x += witchBounds.extents.x/NUMRAINDROPS) {
+				total++;
+				RaycastHit2D hit = Physics2D.Raycast (new Vector2 (x, RAINHEIGHT), Vector2.down);
+				if(hit.collider != null && hit.collider.gameObject == witch.gameObject) {
+					numCollided++;
+				}
 			}
-		}
 
-		float damage = numCollided / total;
+			float damage = numCollided / total;
 
-		if (damage > DAMAGETHRESHOLD) {
-			witch.State = WitchState.Damaged;
+			if (damage > DAMAGETHRESHOLD) {
+				witch.State = WitchState.Damaged;
+			}
 		}
 	}
 }
---
+++ Assets/Scripts/WitchManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WitchManager : MonoBehaviour {
+	public GameObject WitchPrefab;
+	private List<Witch> witches = new List<Witch>();
+	private const float summonEvery = 5f;
+	private float lastSummon = 0f;
+
+	public List<Witch> Witches {
+		get{
+			return witches;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		witches.Add(Instantiate(WitchPrefab).GetComponent<Witch>());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lastSummon += Time.deltaTime;
+		if (lastSummon >= summonEvery) {
+			lastSummon = 0f;
+			witches.Add(Instantiate(WitchPrefab).GetComponent<Witch>());
+		}
+	}
+}
